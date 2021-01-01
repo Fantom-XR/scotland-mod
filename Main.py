d921191bc3d9c5b8b74e7b73634d87dde11e4313
Fantom XR <52836243+Fantom-XR@users.noreply.github.com>
2021-01-01 11:09:01 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -21,7 +21,6 @@ async def ping(ctx):
 async def on_member_join(member):
     chan1 = member.guild.get_channel(794521047558520852)
     await chan1.send(f"**👋 New Member:** {member.mention}, There are now {member.guild.member_count} members in the server.")
-    await chan1.send(f"https://images-ext-2.discordapp.net/external/5G-brBfC9GADnlcviLP0m9bF_V7M8bj2oKSfN6ufcMk/https/media.discordapp.net/attachments/724206682321846312/734300288915079238/Welcome_to_Reserve_Tech_bot_post_ya.jpg")
 
 @bot.event
 async def on_member_remove(member):
@@ -73,14 +72,14 @@ async def suggest(ctx, *, message=None):
 @bot.command()
 async def hub(ctx):
     """|| gives you link to the Hub!"""
-    await ctx.send("https://www.roblox.com/games/5199442577/Hub")
+    await ctx.send("https://www.roblox.com/games/6153453289/AV-Hub")
 
 #link to group
 
 @bot.command()
 async def group(ctx):
     """|| Gives you link the the group!"""
-    await ctx.send("https://www.roblox.com/games/6153453289/AV-Hub")
+    await ctx.send("https://www.roblox.com/groups/8644129/AV-Avenir#!/about")
 
 
 '''@bot.command(name="8ball")
