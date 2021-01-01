2451c34558e595433277bba663cf1fc00d120063
Fantom XR <52836243+Fantom-XR@users.noreply.github.com>
2021-01-01 11:20:51 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -19,8 +19,8 @@ async def ping(ctx):
 
 @bot.event
 async def on_member_join(member):
-    chan1 = member.guild.get_channel(794521047558520852)
-    await chan1.send(f"**👋 New Member:** {member.mention}, There are now {member.guild.member_count} members in the server.")
+    chan1 = member.guild.get_channel(790682444252315668)
+    await chan1.send(f"**Welcome to Avenir!** \n Hello, {member.mention}, welcome to Avenir! We are now at 198 members. \n Please view #information to learn more about us. Before you gain full access to our server, you'll need to verify in <#790682463126945792>. Instructions for verification can be found in <#790682444252315668>.")
 
 @bot.event
 async def on_member_remove(member):
