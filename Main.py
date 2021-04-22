757a26a08102c7806c202de644fb4c07ad6f8da6
Fantom XR <52836243+Fantom-XR@users.noreply.github.com>
2021-04-22 16:20:20 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -10,7 +10,7 @@ bot.remove_command("help")
 @bot.event
 async def on_ready():
     print("signed as Scotland Moderation \n prefix is :")
-    await bot.change_presence(activity=discord.Game(name="with Tyler 🙈"))
+    await bot.change_presence(status=discord.Status.idle, activity=discord.Game(name="with Tyler 🙈"))
 
 @bot.command()
 async def ping(ctx):
