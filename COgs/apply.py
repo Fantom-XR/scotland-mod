19d30d5ca3d5d3d279085a5e2128b3384e6e2939
Fantom XR <52836243+Fantom-XR@users.noreply.github.com>
2021-04-26 14:45:08 +0100

    Update apply.py

--- COgs/apply.py
+++ COgs/apply.py
@@ -7,7 +7,7 @@ from discord.ext.commands.cooldowns import BucketType
 class marketplace(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-Alphabet = ["A", "a", "B", "b", "C", "c", "D", "d", "E", "e", "F", "f", "G", "g", "H", "h", "I", "i", "J", "j", "K", "k", "L", "l", "M", "m", "N", "n", "O", "o", "P", "p", "Q", "q", "R", "r", "S", "s", "T", "t", "U", "u", "V", "v", "W", "w", "X", "x", "Y", "y", "Z", "z"]
+        Alphabet = ["A", "a", "B", "b", "C", "c", "D", "d", "E", "e", "F", "f", "G", "g", "H", "h", "I", "i", "J", "j", "K", "k", "L", "l", "M", "m", "N", "n", "O", "o", "P", "p", "Q", "q", "R", "r", "S", "s", "T", "t", "U", "u", "V", "v", "W", "w", "X", "x", "Y", "y", "Z", "z"]
 
     @commands.command()
     id = random.choice(Alphabet) + random.choice(Alphabet) + random.choice(Alphabet) + random.choice(Alphabet) + random.choice(Alphabet) + random.choice(Alphabet)
