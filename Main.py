81b4f5fb1a4a46ea9b3efc5e9886eff144c6fd54
Fantom XR <52836243+Fantom-XR@users.noreply.github.com>
2021-05-11 13:36:26 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,15 +1,97 @@
 import discord
 from discord.ext import commands, tasks
 from discord.ext.commands.cooldowns import BucketType
+from dotenv import load_dotenv
+import os
 import random
+import aiofiles
+import keep_alive
 
-bot = commands.Bot(command_prefix=commands.when_mentioned_or(':'))
+load_dotenv()  # Load environment variables from .env file.
+
+
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('.'), case_insensitive=True, intents=discord.Intents.all())
 bot.remove_command("help")
+bot.warnings = {} # guild_id : {member_id: [count, [(admin_id, reason)]]}
 
 @bot.event
 async def on_ready():
     await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.playing, name="with Tyler 🙈"))
 
+@bot.event
+async def on_ready():
+    for guild in bot.guilds:
+        bot.warnings[guild.id] = {}
+        
+        async with aiofiles.open(f"{guild.id}.txt", mode="a") as temp:
+            pass
+
+        async with aiofiles.open(f"{guild.id}.txt", mode="r") as file:
+            lines = await file.readlines()
+
+            for line in lines:
+                data = line.split(" ")
+                member_id = int(data[0])
+                admin_id = int(data[1])
+                reason = " ".join(data[2:]).strip("\n")
+
+                try:
+                    bot.warnings[guild.id][member_id][0] += 1
+                    bot.warnings[guild.id][member_id][1].append((admin_id, reason))
+
+                except KeyError:
+                    bot.warnings[guild.id][member_id] = [1, [(admin_id, reason)]] 
+    
+    print(bot.user.name + " is ready.")
+
+@bot.event
+async def on_guild_join(guild):
+    bot.warnings[guild.id] = {}
+
+@bot.command()
+@commands.has_permissions(manage_messages = True)
+async def warn(ctx, member: discord.Member=None, *, reason=None):
+    if member is None:
+        return await ctx.send("The provided member could not be found or you forgot to provide one.")
+        
+    if reason is None:
+        return await ctx.send("Please provide a reason for warning this user.")
+
+    try:
+        first_warning = False
+        bot.warnings[ctx.guild.id][member.id][0] += 1
+        bot.warnings[ctx.guild.id][member.id][1].append((ctx.author.id, reason))
+
+    except KeyError:
+        first_warning = True
+        bot.warnings[ctx.guild.id][member.id] = [1, [(ctx.author.id, reason)]]
+
+    count = bot.warnings[ctx.guild.id][member.id][0]
+
+    async with aiofiles.open(f"{ctx.guild.id}.txt", mode="a") as file:
+        await file.write(f"{member.id} {ctx.author.id} {reason}\n")
+
+    await ctx.send(f"{member.mention} has {count} {'warning' if first_warning else 'warnings'}.")
+
+@bot.command()
+@commands.has_permissions(manage_messages = True)
+async def warnings(ctx, member: discord.Member=None):
+    if member is None:
+        return await ctx.send("The provided member could not be found or you forgot to provide one.")
+    
+    embed = discord.Embed(title=f"Displaying Warnings for {member.name}", description="", colour=discord.Colour.red())
+    try:
+        i = 1
+        for admin_id, reason in bot.warnings[ctx.guild.id][member.id][1]:
+            admin = ctx.guild.get_member(admin_id)
+            embed.description += f"**Warning {i}** given by: {admin.mention} for: *'{reason}'*.\n"
+            i += 1
+
+        await ctx.send(embed=embed)
+
+    except KeyError: # no warnings
+        await ctx.send("This user has no warnings.")
+    
 @bot.command()
 async def ping(ctx):
     """|| Tells the bot's latency """
@@ -114,4 +196,4 @@ bot.load_extension("COgs.sfrs")
 bot.load_extension("COgs.nhs")
 bot.load_extension("COgs.psoscommands")
 
-bot.run("ODE0ODU3MTEyNDU4MTAwNzM2.YDj80g.pC1y73miU8JS1B2h-nN86e9ouDw")
+bot.run(os.getenv("DISCORDTOKEN"))
